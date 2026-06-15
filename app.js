8ec7d2749b047fe55de1f0a0c7c2065806ee4ebc
saulo.gomes <saulo.gomes@aluno.edu.pi.gov.br>
2026-06-15 15:01:47 +0000

    calculadora.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const calculadora = require ('./calculadora.js')
+let resultadoSoma = calculadora.somar (10, 20);
+console.log (resultadoSoma);
+
+let resultadoSubitrair = calculadora.subtrair (10, 5)
+console.log (resultadoSubitrair);
+
+let resultadoDividir = calculadora.dividir (20, 2)
+console.log (resultadoDividir);
+
+let resultadoMultiplicar = calculadora.multiplicar (40 , 2)
+console.log (resultadoMultiplicar);
