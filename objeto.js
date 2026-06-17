44eade6a786b7b81ba9d3a92f50f67e841f00cf0
saulo.gomes <saulo.gomes@aluno.edu.pi.gov.br>
2026-06-17 13:54:48 +0000

    objeto.js

---
+++ objeto.js
@@ -0,0 +1,21 @@
+let variavel = visualViewport;
+const pessoa= { 
+    nome : "joão",
+    Idade: 26,
+    peso : 85,
+    altura : 180,
+    sexo : "masculino",
+
+
+
+}
+
+    console.log (pessoa.nome);
+    pessoa.idade = 26
+    console.log (pessoa.idade);
+    pessoa.peso = 85
+    console.log (pessoa.peso);
+    pessoa.peso = 85
+    console.log (pessoa.altura);
+    console.log (pessoa.sexo);
+    
